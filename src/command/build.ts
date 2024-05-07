c16cd4b8af2a6be9a2ca497a55ddcff132e92015
millionkn <millionkn@sina.com>
2024-05-07 17:56:18 +0800

    修正,现在build时会读取tsconfig以支持alias

--- src/command/build.ts
+++ src/command/build.ts
@@ -12,27 +12,57 @@ export function appendBuild(cac: CAC) {
       mode: string | undefined,
     }) => {
       const mode = options.mode || 'production'
+      const configFile = service.ts.findConfigFile('', service.ts.sys.fileExists)
+      if (!configFile) {
+        throw new Error(`can't find 'tsconfig.json'`)
+      }
+      const readResult = service.ts.readConfigFile(configFile, service.ts.sys.readFile)
+      if (!readResult.config) {
+        console.error(readResult.error?.messageText)
+        throw new Error()
+      }
       await build({
         plugins: [
+          readResult.config.compilerOptions.declaration && dts(),
           nodePolyfills(),
-          service.config.options.declaration && dts({ compilerOptions: service.config.options }),
+          {
+            name: 'ts-resolver',
+            config: () => {
+              return {
+                build: {
+                  rollupOptions: {
+                    external: (source, importer) => {
+                      if (!importer) { return false }
+                      const { resolvedModule } = service.ts.resolveModuleName(source, fileURLToPath(new URL(pathToFileURL(importer))), service.config.options, service.ts.sys)
+                      if (!resolvedModule) { return true }
+                      return resolvedModule.isExternalLibraryImport
+                    }
+                  }
+                },
+              }
+            },
+            resolveId: function (source, importer) {
+              if (!importer) { return }
+              const url = pathToFileURL(importer)
+              const path = fileURLToPath(url)
+              const { resolvedModule } = service.ts.resolveModuleName(source, path, service.config.options, service.ts.sys)
+              if (!resolvedModule) { return }
+              if (resolvedModule.isExternalLibraryImport) { return }
+              return {
+                id: resolvedModule.resolvedFileName,
+                external: false,
+              }
+            }
+          },
         ],
         envFile: false,
         mode,
         build: {
           lib: {
             formats: ['es'],
-            entry: target,
+            entry: { index: target },
           },
           minify: false,
-          rollupOptions: {
-            external: (source, importer) => {
-              if (!importer) { return false }
-              const { resolvedModule } = service.ts.resolveModuleName(source, fileURLToPath(new URL(pathToFileURL(importer))), service.config.options, service.ts.sys)
-              if (!resolvedModule) { return true }
-              return resolvedModule.isExternalLibraryImport
-            }
-          },
         }
       })
     })
