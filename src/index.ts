7f5e5a2e5c54707839ffebd692faab3e5d4af89b
millionkn <millionkn@sina.com>
2024-05-08 11:42:22 +0800

    无参数时显示帮助信息并退出

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import createCAC from 'cac'
 import { appendRun } from './command/run.js'
 import { appendBuild } from './command/build.js'
+import { exit } from 'process'
 
 const cac = createCAC('tss')
 appendRun(cac)
 appendBuild(cac)
+cac.command('').action(() => {
+  cac.outputHelp()
+  exit(1)
+})
 cac.help().parse()
